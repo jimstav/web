3ba972b231937ddabecbb197fee88bc3702235af
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-31 23:41:11 +0300

    Saving Data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ id: 1, name: 'John Doe', age: 10 });
 
 user.on('change', () => {
   console.log('User was changed', user);
 });
 
-user.fetch();
+user.on('save', () => {
+  console.log('User was saved', user);
+});
+
+// user.fetch();
+user.save();
