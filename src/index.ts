5979e9950d452214162bea1c9b6c0be86720e39a
Dimitris Stavroulakis <jimstav@gmail.com>
2025-04-03 20:06:14 +0300

    Final User Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'John Doe', age: 10 });
+const user = User.buildUser({ id: 1, name: 'John Doe', age: 10 });
 
 user.on('change', () => {
   console.log('User was changed', user);
